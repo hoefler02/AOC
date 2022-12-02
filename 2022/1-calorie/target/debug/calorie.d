04eadba718d87b71fac87f3f44c27d905c6dd197
Michael Hoefler <hoefler2002@gmail.com>
2022-12-01 19:55:48 -0500

    day 1

---
+++ 2022/1-calorie/target/debug/calorie.d
@@ -0,0 +1,1 @@
+/home/michael/Code/AOC/1-calorie/target/debug/calorie: /home/michael/Code/AOC/1-calorie/src/main.rs
---
+++ 2022/1-calorie/target/debug/deps/calorie-fb3d670fc7d3a1cf.d
@@ -0,0 +1,5 @@
+/home/michael/Code/AOC/calorie/target/debug/deps/calorie-fb3d670fc7d3a1cf: src/main.rs
+
+/home/michael/Code/AOC/calorie/target/debug/deps/calorie-fb3d670fc7d3a1cf.d: src/main.rs
+
+src/main.rs:
