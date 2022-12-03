99903bbed8c46214a75ff5892fc94dbbd7971952
Michael Hoefler <hoefler2002@gmail.com>
2022-12-02 22:02:57 -0500

    remove target

--- target/debug/AOC.d
+++
@@ -1,1 +0,0 @@
-/home/michael/Code/AOC/target/debug/AOC: /home/michael/Code/AOC/src/01-calorie.rs /home/michael/Code/AOC/src/02-rock-paper-scissors.rs /home/michael/Code/AOC/src/main.rs
--- target/debug/deps/AOC-4f99e5f9a871fa69.d
+++
@@ -1,7 +0,0 @@
-/home/michael/Code/AOC/target/debug/deps/AOC-4f99e5f9a871fa69: src/main.rs src/./01-calorie.rs src/./02-rock-paper-scissors.rs
-
-/home/michael/Code/AOC/target/debug/deps/AOC-4f99e5f9a871fa69.d: src/main.rs src/./01-calorie.rs src/./02-rock-paper-scissors.rs
-
-src/main.rs:
-src/./01-calorie.rs:
-src/./02-rock-paper-scissors.rs:
